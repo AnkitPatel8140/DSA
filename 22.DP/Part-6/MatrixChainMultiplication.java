cd6a8171126bb582f2d7a24ed2b4afe51a51e265
Ankit399648 <ap806455@gmail.com>
2026-05-17 12:32:00 +0530

    Update MatrixChainMultiplication.java

--- 22.DP/Part-6/MatrixChainMultiplication.java
+++ 22.DP/Part-6/MatrixChainMultiplication.java
@@ -1,3 +1,6 @@
+
+import java.util.Arrays;
+
 public class MatrixChainMultiplication {
     public static int recursion(int[] arr, int i, int j) {
         if(i == j) {
@@ -15,8 +18,34 @@ public class MatrixChainMultiplication {
 
         return ans;
     }
+
+    public static int memoization(int[] arr, int i, int j, int[][] dp) {
+        if(i == j) {
+            return 0;
+        }
+
+        if(dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
+        int ans = Integer.MAX_VALUE;
+
+        for(int k = i; k < j; k++) {
+            int cost1 = memoization(arr, i, k, dp);
+            int cost2 = memoization(arr, k+1, j, dp);
+            int cost3 = arr[i-1]*arr[k]*arr[j];
+            ans = Math.min(ans, cost1 + cost2 + cost3);
+        }
+
+        dp[i][j] = ans;
+        return dp[i][j];
+    }
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,3};
+        int[][] dp = new int[arr.length][arr.length];
+        for(int[] n : dp) Arrays.fill(n, -1);
+        
         System.out.println(recursion(arr, 1, 4));
+        System.out.println(memoization(arr, 1, arr.length-1, dp));
     }
 }
